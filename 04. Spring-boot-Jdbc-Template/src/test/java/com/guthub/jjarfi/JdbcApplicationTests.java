b9365b21a25d8c48041b6821183f49e47b6bb391
root <root@localhost.localdomain>
2020-02-22 20:19:18 +0900

    commit ke empat

---
+++ 04. Spring-boot-Jdbc-Template/src/test/java/com/guthub/jjarfi/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guthub.jjarfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
